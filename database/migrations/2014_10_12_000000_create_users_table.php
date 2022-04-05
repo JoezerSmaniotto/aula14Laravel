7c5aa291954f9b095f1074282274a4ccf3006be8
joezerSmaniotto <joezersmaniotto5@gmail.com>
2022-04-04 22:03:28 -0300

    Alteração no seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('cpf');
+            $table->text('endereco');
+            $table->text('celular');
             $table->rememberToken();
             $table->timestamps();
         });
